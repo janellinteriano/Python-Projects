0b5916f7c05cd2d65bcd9698e477c4de600bc436
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-10-04 18:17:58 -0400

    created class.py

---
+++ class.py
@@ -0,0 +1,13 @@
+class Person: # parent class called "Person"
+    name = ' '
+    age = ' '
+    school = 'Terry High School'
+
+class Student(Person): # child class inherits parent class and has 2 attributes
+    grade = '11th'
+    elective = 'Choir'
+
+class Teacher(Person): # child class inherits parent class and has 2 attributes
+    subject = 'Math'
+    roomNumber = 'J105'
+    
